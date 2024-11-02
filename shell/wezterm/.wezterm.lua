69902e4427e4c19de1a9c98bdf5050c8e06d258f
Johannes Alnes <johalnes@me.com>
2024-11-02 22:09:07 +0100

    Aerospace is app, not tool

--- shell/wezterm/.wezterm.lua
+++ shell/wezterm/.wezterm.lua
@@ -19,9 +19,7 @@ config.colors = {
 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 
-return {
-    font = wezterm.font("MesloLGS NF"),
-}
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 19
 
 config.enable_tab_bar = false
